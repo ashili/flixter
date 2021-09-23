81d11edc502e716c0028fbd584c4316b40d2eaa6
ashili <anisshili.as@gmail.com>
2021-09-23 03:57:24 -0400

    added youtube player

--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,22 +3,40 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overView;
     String backdropPath;
+    double rating;
+    int movieId;
+
+
+    //needed for the Parcel Library
+    public Movie(){}
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 
     public Movie (JSONObject jsonObject) throws JSONException {
         posterPath  = jsonObject.getString("poster_path");
         title  = jsonObject.getString("title");
         overView  = jsonObject.getString("overview");
         backdropPath  = jsonObject.getString("backdrop_path");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
 
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
